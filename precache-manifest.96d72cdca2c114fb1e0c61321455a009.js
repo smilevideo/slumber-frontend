fce8988c5cabe12c443a118be1b954f6a2cd221b
andrewkim <andrewk1216@gmail.com>
2020-02-04 23:02:11 -0500

    Updates

---
+++ precache-manifest.96d72cdca2c114fb1e0c61321455a009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee7d5ef279668e009236c09f3befa58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eced464708682193a55",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "b53c9cfddaa8cbafd616",
+    "url": "/static/css/main.dc147060.chunk.css"
+  },
+  {
+    "revision": "6eced464708682193a55",
+    "url": "/static/js/2.d424febe.chunk.js"
+  },
+  {
+    "revision": "b53c9cfddaa8cbafd616",
+    "url": "/static/js/main.37edd0f8.chunk.js"
+  },
+  {
+    "revision": "6cab334acf7d9daca9dc",
+    "url": "/static/js/runtime-main.d14e69ab.js"
+  }
+]);
